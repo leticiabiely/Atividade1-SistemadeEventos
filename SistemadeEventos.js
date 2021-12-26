acd8a1d601a99876cee29da2e271d8cd93406fa1
Leticia Gabriele <leticiagabriele82@gmail.com>
2021-12-26 20:41:58 -0300

    adicionando o bloco de notas e o sistema de eventos

---
+++ SistemadeEventos.js
@@ -0,0 +1,34 @@
+console.log ('Sejam Bem Vindos à Lett Eventos!')
+
+let DatadoEvento = 09/01/2022
+
+if(DatadoEvento > 05/01/2022) {
+    console.log ("Cadastro do evento permitido.") 
+    
+    let IdadedoUsuário = 18
+
+    if (IdadedoUsuário > 17) {
+        console.log ("Permitir Cadastro.")
+    } else
+        console.log ("Idade não permitida, cadastro não concluído!")
+
+} else
+    console.log ("Cadastro não permitido por data inválida.")
+
+
+    let listadeParticipantes = ["ana", "Matheus", "Julia",]
+      console.log ("listar participantes e palestrantes", listadeParticipantes, listadeParticipantes.length)
+
+
+    if (listadeParticipantes.length < 99) {
+        console.log ("Quantidade de participantes ok. Cadastro permitido.")
+    } else 
+      console.log ("Quantidade excedida, limite de 100 pessoas. Casdastro não concluído.")
+
+      
+
+      
+
+
+
+   
